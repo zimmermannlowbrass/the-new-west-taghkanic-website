33b6f15dedcb41371fff93f0a8244c9cf2680e20
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-05 16:47:39 -0500

    more details on cards

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 function Item( { item }) {
+
+    const ingredientsList = item.ingredients.map(ingredient => {
+        return (
+            <ul key={item.ingredients.indexOf(ingredient)}>{ingredient}</ul>
+        )
+    })
+
     return (
-        <div>
-            {item.name}
+        <ul className="card">
+            <h3>{item.name}</h3>
+            <p>Price: {item.price}</p>
+            <h5>Ingredients: {ingredientsList}</h5>
             <br></br>
             <img src={item.image} alt={item.name} />
-        </div>
+        </ul>
     )
 }
 
