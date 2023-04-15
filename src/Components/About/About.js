41021a107acfb7562a94d7e54ccbb5be89b61f5e
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-15 12:40:38 -0500

    lots of CSS

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -25,8 +25,8 @@ function About() {
                 {aboutRenderList}
             </div>
             <br />
-            <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11837.74756962173!2d-73.7173729!3d42.1195294!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89dd9ba86b1bda47%3A0xda77ae7251255f82!2sWest%20Taghkanic%20Diner!5e0!3m2!1sen!2sus!4v1680817898096!5m2!1sen!2sus" width={"600" }height={"400"} style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-            <img className='dinerIcon'src={require ('../dinerPics/dinerIcon.jpeg')} alt="diner" width={"600" }height={"400"}  />
+            <iframe className="aboutImage" title="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11837.74756962173!2d-73.7173729!3d42.1195294!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89dd9ba86b1bda47%3A0xda77ae7251255f82!2sWest%20Taghkanic%20Diner!5e0!3m2!1sen!2sus!4v1680817898096!5m2!1sen!2sus" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <img className='aboutImage'src={require ('../dinerPics/dinerIcon.jpeg')} alt="diner"/>
             <br />
         </div>
     )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,12 @@ import React from "react";
 function Home() {
     return(
         <div>
-            <h2>The WTD stands as originally constructed, with the same mission as nearly 65 years ago — to provide a gathering spot for hungry passerby and long time residents alike.</h2>
+            <h2>The WTD stands as originally constructed, with the same mission as nearly 65 years ago...<br /> to provide a gathering spot for hungry passerby and long time residents alike.</h2>
             <br />
             <img className='dinerPhotos'src={require ('./dinerPics/diner2.jpg')} alt="diner" />
             <img className='dinerPhotos'src={require ('./dinerPics/diner3.jpg')} alt="diner" />
             <img className='dinerPhotos'src={require ('./dinerPics/diner4.jpg')} alt="diner" />
+            <br />
             <a id="link" href='https://www.wtdinerny.com/'>Link to the actual West Taghkanic Diner</a>
         </div>
     )
