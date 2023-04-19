45d070097a3da4e2173290cee75553c03455be2f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-18 19:55:15 -0500

    test

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,10 @@ function Home() {
     return(
         <div>
             <h1 className="subSectionTitle">Home Section</h1>
-            <h2 className="textBox">The West Taghkanic Diner (WTD) stands as originally constructed, with the same mission as nearly 65 years ago...<br /> To this day, it continues to provide a gathering spot for hungry passerby and long time residents alike.</h2>
+            <h2 className="textBox">
+                The West Taghkanic Diner (WTD) stands as originally constructed, with the same mission as nearly 65 years ago...
+                <br /> 
+                To this day, it continues to provide a gathering spot for hungry passerby and long time residents alike.</h2>
             <br />
             <img className='dinerPhotos'src={require ('../dinerPics/diner2.jpg')} alt="diner" />
             <img className='dinerPhotos'src={require ('../dinerPics/diner3.jpg')} alt="diner" />
